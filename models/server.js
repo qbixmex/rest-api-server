c9d0339a010b38435686f99375ad990d60e8ae34
Daniel González Briseño <qbixmex@gmail.com>
2022-04-15 02:29:36 -0500

    Lesson 03 - Response and Status Codes

--- models/server.js
+++ models/server.js
@@ -19,7 +19,29 @@ class Server {
 
   routes() {
     this.app.get("/api", (request, response) => {
-      response.send("Express Running");
+      response.json({
+        msg: "Get Api"
+      });
+    });
+    this.app.patch("/api", (request, response) => {
+      response.json({
+        msg: "Patch Api"
+      });
+    });
+    this.app.patch("/api", (request, response) => {
+      response.json({
+        msg: "Patch Api"
+      });
+    });
+    this.app.post("/api", (request, response) => {
+      response.json({
+        msg: "Post Api"
+      });
+    });
+    this.app.delete("/api", (request, response) => {
+      response.json({
+        msg: "Delete Api"
+      });
     });
   }
 
