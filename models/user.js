cf5c7af0bb7de72e53939e275d9f02c6d095d2ce
Daniel González Briseño <daniel.gb@globant.com>
2022-04-19 12:21:12 -0500

    Lesson 25 - _id renamed to uid

--- models/user.js
+++ models/user.js
@@ -37,7 +37,11 @@ const UserSchema = Schema({
 });
 
 UserSchema.methods.toJSON = function() {
-  const { __v, password, ...user } = this.toObject();
+  const { __v, password, _id, ...user } = this.toObject();
+
+  // _id --> renamed to uid
+  user.uid = _id;
+
   return user;
 };
 
