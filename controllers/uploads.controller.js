106f0f60790b42a53a83e64a53ca9c0c23baa03f
Daniel González Briseño <daniel.gb@globant.com>
2022-04-21 18:35:10 -0500

    Lesson 47 - Upload File Helper

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,41 +1,16 @@
-const path = require('path');
 const { response, request } = require('express');
-const { v4: uuidv4 } = require('uuid');
+const { uploadFile } = require('../helpers');
 
-const loadFile = (req = request, res = response) => {
+const loadFile = async (req = request, res = response) => {
   // Check if no files were provided
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
     res.status(400).send('No files were uploaded.');
     return;
   }
 
-  // Destructuring file from files object
-  // Note file name is given by front end form or
-  // in this case by postman
-  const { file } = req.files;
+  const filename = await uploadFile(req.files);
 
-  const fileSplitted = file.name.split('.');
-  const extension = fileSplitted[fileSplitted.length - 1];
-
-  const validExtensions = [ 'jpg', 'jpeg', 'png', 'gif' ];
-
-  if (!validExtensions.includes(extension)) {
-    return res.status(400).json({
-      msg: `File extension .${extension} is not valid, only (${ validExtensions }) are valid!`
-    });
-  };
-
-  // Unique File Name with UUID
-  const uniqueFileName = uuidv4() + '.' + extension; 
-
-  // Path where to upload file
-  const uploadPath = path.join(__dirname, '../uploads/', uniqueFileName);
-
-  // Perform file uploading
-  file.mv(uploadPath, error => {
-    if ( error ) return res.status(500).json({ error });
-    res.json({ msg: `File (${file.name}) uploaded to: ${ uploadPath }` });
-  });
+  res.json({ filename });
 };
 
 module.exports = {
---
+++ helpers/upload-file.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+/**
+ * Upload file to server
+ * @param {fileUpload.FileArray} files Nomrally is req.files
+ * @param {string[]} validExtensions By default is [ 'jpg', 'jpeg', 'png', 'gif' ].
+ * @param {string} folder You can set a custom folder inside uploads at root directory
+ * @returns {Promise<string>} File Path
+ */
+const uploadFile = (files, validExtensions = [ 'jpg', 'jpeg', 'png', 'gif' ], folder = '') => {
+  return new Promise((resolve, reject) => {
+    // Destructuring file from files object
+    // Note file name is given by front end form or
+    // in this case by postman
+    const { file } = files;
+
+    const fileSplitted = file.name.split('.');
+    const extension = fileSplitted[fileSplitted.length - 1];
+
+    if (!validExtensions.includes(extension)) {
+      reject(`File extension .${extension} is not valid, only (${ validExtensions }) are valid!`);
+      return;
+    };
+
+    // Unique File Name with UUID
+    const uniqueFileName = uuidv4() + '.' + extension; 
+
+    // Path where to upload file
+    const uploadPath = path.join(__dirname, '../uploads/', folder, uniqueFileName);
+
+    // Perform file uploading
+    file.mv(uploadPath, error => {
+      if ( error ) return reject(error);
+      resolve(file.name);
+    });
+  });
+};
+
+module.exports = { uploadFile };
